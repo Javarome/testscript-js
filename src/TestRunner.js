5349fffdc535b1bed6549c2a75917332d9d47d4b
javarome <javarome@gmail.com>
2023-11-17 10:57:41 +0100

    add exclude array option

--- src/TestRunner.js
+++ src/TestRunner.js
@@ -1,7 +1,7 @@
-import {promise as glob} from "glob-promise"
-import path from "path"
-import {DefaultLogger, Logger} from "./log/index.js"
-import {AnsiColor} from "./AnsiColor.js"
+import { promise as glob } from 'glob-promise'
+import path from 'path'
+import { DefaultLogger, Logger } from './log/index.js'
+import { AnsiColor } from './AnsiColor.js'
 
 export class TestRunnerResult {
   /**
@@ -53,6 +53,15 @@ export class TestRunner {
    */
   #numberFormat
 
+  static toArray (pattern) {
+    pattern = pattern.trim()
+    if (pattern.startsWith('{') && pattern.endsWith('}')) {
+      let files = pattern.substring(1, pattern.length - 1)
+      return files.split(',')
+    }
+    return [pattern]
+  }
+
   /**
    *
    * @param {string} includePattern
@@ -60,14 +69,14 @@ export class TestRunner {
    * @param {Logger} logger
    * @param {Intl.NumberFormat} numberFormat
    */
-  constructor(
-    includePattern = "**/*Test.js",
-    excludePattern = "node_modules/**/*.*",
-    logger = new DefaultLogger("testscript"),
-    numberFormat = new Intl.NumberFormat(undefined, {maximumFractionDigits: 2})
+  constructor (
+    includePattern = '**/*Test.js',
+    excludePattern = 'node_modules/**/*.*',
+    logger = new DefaultLogger('testscript'),
+    numberFormat = new Intl.NumberFormat(undefined, { maximumFractionDigits: 2 })
   ) {
     this.#includePattern = includePattern
-    this.#excludePattern = excludePattern
+    this.#excludePattern = TestRunner.toArray(excludePattern)
     this.logger = logger
     this.#numberFormat = numberFormat
   }
@@ -75,9 +84,9 @@ export class TestRunner {
   /**
    * @return {Promise<TestRunnerResult>}
    */
-  async run() {
+  async run () {
     const runStart = performance.now()
-    const files = await glob(this.#includePattern, {ignore: this.#excludePattern})
+    const files = await glob(this.#includePattern, { ignore: this.#excludePattern })
     const suites = []
     let success = true
     for (const filePath of files) {
@@ -87,19 +96,19 @@ export class TestRunner {
     }
     const runEnd = performance.now()
     const duration = runEnd - runStart
-    return {suites, duration}
+    return { suites, duration }
   }
 
   /**
    * @param {string} file
    * @return Promise<SuiteResult>
    */
-  async runSuite(file) {
+  async runSuite (file) {
     const testStart = performance.now()
     let testEnd
     let error
     try {
-      this.logger.debug("Executing", file)
+      this.logger.debug('Executing', file)
       const test = path.join(process.cwd(), file)
       await import(test)
     } catch (e) {
@@ -111,31 +120,31 @@ export class TestRunner {
     let status
     let details
     if (error) {
-      status = AnsiColor.str("FAIL", AnsiColor.fgRed)
+      status = AnsiColor.str('FAIL', AnsiColor.fgRed)
       let stack = error.stack
       if (stack) {
-        let filePattern = file.replaceAll("/", "\/")
+        let filePattern = file.replaceAll('/', '\/')
         const errorRegExp = new RegExp(`.*(Error: (.+)\\n)[\\s\\S]*?${filePattern}:\\d+:\\d+\\)\n([\\s\\S]*)`,
-          "gm")
+          'gm')
         let items = errorRegExp.exec(stack)
         if (items && items.length > 0) {
-          details = AnsiColor.str(items[2] + "\n" + items[3], AnsiColor.fgRed)
+          details = AnsiColor.str(items[2] + '\n' + items[3], AnsiColor.fgRed)
         }
       }
     } else {
-      status = AnsiColor.str("PASS", AnsiColor.fgGreen)
+      status = AnsiColor.str('PASS', AnsiColor.fgGreen)
       details = AnsiColor.str(`(${this.durationStr(duration)})`, AnsiColor.fgWhite)
     }
-    this.logger.log(status, file, details || "")
-    return {file, duration, error}
+    this.logger.log(status, file, details || '')
+    return { file, duration, error }
   }
 
   /**
    * @param {number} value
    * @return {string}
    */
-  durationStr(value) {
-    return this.#numberFormat.format(value) + " ms"
+  durationStr (value) {
+    return this.#numberFormat.format(value) + ' ms'
   }
 
   /**
@@ -143,7 +152,7 @@ export class TestRunner {
    * @param {TestRunnerResult} result
    * @return {boolean}
    */
-  allSucceeded(result) {
+  allSucceeded (result) {
     const successCount = this.successCount(result)
     const total = result.suites.length
     return successCount === total
@@ -154,7 +163,7 @@ export class TestRunner {
    * @param {TestRunnerResult} result
    * @return {number}
    */
-  successCount(result) {
+  successCount (result) {
     return result.suites.reduce((count, suite) => {
       count += suite.error ? 0 : 1
       return count
